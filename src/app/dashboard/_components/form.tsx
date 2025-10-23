45ca850ee97eedb9cde68c799ebe1ef844934f7b
Sohal_Sawardekar <sohalsawardekar11@gmail.com>
2025-10-23 14:04:31 +0530

    added auth UI

---
+++ src/app/dashboard/_components/form.tsx
@@ -0,0 +1,12 @@
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
+
+export default function ComplainForm() {
+  return (
+    <Card>
+      <CardContent>
+        <CardTitle>Dashboard</CardTitle>
+        <p>Welcome to the dashboard!</p>
+      </CardContent>
+    </Card>
+  )
+}
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,6 @@
+import ComplainForm from "./_components/form"
+export default function DashboardPage() {
+  return (
+    <ComplainForm />
+  )
+}
